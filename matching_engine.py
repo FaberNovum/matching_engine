d14f894f563a74d1cbb56e377609a1a1b4be6282
Taylor <wrighttaylor67@gmail.com>
2021-07-14 19:16:27 -0500

    Continued fleshing out match function

--- matching_engine.py
+++ matching_engine.py
@@ -1,5 +1,8 @@
 import sortedcontainers
 
+from threading import Thread
+from collections import deque
+
 class Order:
     def __init__(self, order_type, side, price, quantity):
         self.type = order_type
@@ -67,3 +70,59 @@ class OrderBook:
         plt.show()
         if save:
             fig.savefig('plot.png', transparent=True)
+
+class MatchingEngine:
+    def __init__(self, threaded=False):
+        self.queue = deque()
+        self.orderbook = Orderbook()
+        self.trades = deque()
+        self.threaded = threaded
+        if self.threaded:
+            self.thread = Thread(target=self.run)
+            self.thread.start()
+
+    def process(self, order):
+        if self.threaded:
+            self.queue.append(order)
+        else:
+            self.match(order)
+
+    def get_trades(self):
+        trades = list(self.trades)
+        return trades
+
+    def match(self, order):
+        if order.side == 'buy' and order.price >= self.orderbook.best_ask():
+            # Buy order crossed the spread
+            filled = 0
+            consumed_asks =[]
+            for i in range(len(self.orderbook.asks)):
+                ask = self.orderbook.asks[i]
+
+                if ask.price > order.price:
+                    break # Price of ask is too high, stop filling the order
+                elif filled == order.quantity:
+                    break # Order filled
+
+                if filled + ask.quantity <= order.quantity: # order not yet filled, ask will consumed as whole
+                    filled += ask.quantity
+                    trade = Trade(ask.price, ask.quantity)
+                    self.trades.append(trade)
+                    consumed_asks.append(ask)
+                elif filled + ask.quantity > order.quantity: # order is filled, ask willl be consumed partially
+                    volume = order.quantity-filled
+                    filled += volume
+                    trade = Trade(ask.price, volume)
+                    self.trades.append(trade)
+                    ask.quantity -= volume
+            # Place any remaining volume in Limit Order Book
+            if filled < order.quantity:
+                self.orderbook.add(Order("limit", "buy", order.price, order.quantity-filled))
+                
+            # Remove asks used for filling order
+            for ask in consumed_asks:
+                self.orderbook.remove(ask)
+        elif order.side == 'sell' and order.price <= self.orderbook.best_bid():
+            # Sell order crossed the spread
+            filled = 0
+            consumed_bids = []
